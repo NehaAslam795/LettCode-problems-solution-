27544bec4e73cea8ca25b58c051baa90953da3c1
NehaAslam795 <nehatech2007@gmail.com>
2025-11-29 00:43:05 +0500

    Time: 7 ms (34.56%) | Memory: 45.1 MB (47.79%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                switch (token) {
+                    case "+": stack.push(a + b); break;
+                    case "-": stack.push(a - b); break;
+                    case "*": stack.push(a * b); break;
+                    case "/": stack.push(a / b); break; // integer division
+                }
+            } else {
+                stack.push(Integer.parseInt(token)); // number
+            }
+        }
+
+        return stack.pop(); // result
+    }
+}
